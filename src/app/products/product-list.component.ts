e76faa80fb56e0bf3a1cfa60536bbd24a549aa8a
Gianni Fontanot <giannifontanot@gmail.com>
2022-07-02 18:49:25 -0500

    Interface implemented and also new custom pipe.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {IProduct} from "./products";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = 'cart';
-  products: any [] = [
+  products: IProduct [] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -43,5 +45,9 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+console.log("This is OnInit")
   }
 }
